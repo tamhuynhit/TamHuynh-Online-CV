3197a69ea73aaaa678cd2c8513d22ac4b372ef80
Bang Dao <bang.dao@eastagile.com>
2016-07-13 16:01:46 +0700

    Update contact bottom sheet

--- js/app.js
+++ js/app.js
@@ -17,11 +17,6 @@ module.config(function ($routeProvider, $mdThemingProvider) {
       controller: 'ExperienceController',
       title: 'Experience'
     })
-    .when('/contact', {
-      templateUrl: 'templates/contact.html',
-      controller: 'ContactController',
-      title: 'Contact'
-    })
     .otherwise({
       redirectTo: '/profile'
     });
@@ -35,31 +30,63 @@ module.run(['$location', '$rootScope', function($location, $rootScope) {
     $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
         if (current.hasOwnProperty('$$route')) {
             $rootScope.title = current.$$route.title;
-            // $('.button-collapse').sideNav('hide');
         }
     });
 }]);
 
-module.controller('MainController', function($scope, $mdSidenav, $location) {
+module.controller('MainController', function($scope, $mdSidenav, $location, $mdBottomSheet) {
   $scope.toggleSideNav = function() {
     $mdSidenav('left-sidenav').toggle();
   };
 
   $scope.changeView = function(view){
-    $location.path(view); // path not hash
+    $location.path(view);
   };
+
+  $scope.toggleContact = function() {
+    $mdBottomSheet.show({
+      controller: 'ContactController',
+      templateUrl: './templates/contact.html',
+      parent: angular.element(document.querySelector('body'))
+    });
+  }
 });
 
 module.controller('ProfileController', function($scope) {
-  $scope.message = 'Profile'
+  $scope.message = 'Profile';
 });
 
 module.controller('EducationController', function($scope) {
-  $scope.message = 'Education'
+  $scope.message = 'Education';
 });
 
 module.controller('ExperienceController', function($scope) {
+  $scope.message = 'Experience';
 });
 
-module.controller('ContactController', function($scope) {
+module.controller('ContactController', function($scope, $mdBottomSheet) {
+  $scope.contacts = [
+    { title:'Phone', 
+      content: [
+        { name: 'Phone', icon:'phone', content:'+84-936471994' }
+      ]
+    },
+    { title:'Email', 
+      content: [
+        { name: 'Outlook', icon:'envelope-o', content:'tamhuynh.msp@outlook.com' },
+        { name: 'Gmail', icon:'envelope-o', content:'tamhuynh0407@gmail.com' }
+      ]
+    },
+    { title:'Social', 
+      content: [
+        { name: 'Skype', icon:'skype', content:'tamhuynh.0407' },
+        { name: 'Linkedin', icon:'linkedin-square', content:'www.linkedin.com/in/tamhuynhit' },
+        { name: 'Github', icon:'github', content:'www.github.com/tam0407' }
+      ]
+    }
+  ];
+
+  $scope.performContact = function(type) {
+    $mdBottomSheet.hide();
+  }
 });
